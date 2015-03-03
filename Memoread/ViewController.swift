0c54fc2e54199e30181af593beeb9802171d5d42
tidalboot <tidalboot@googlemail.com>
2015-03-03 09:38:43 +0000

    Initial Commit

---
+++ Memoread/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memoread
+//
+//  Created by Nick Jones on 03/03/2015.
+//  Copyright (c) 2015 Nick Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
